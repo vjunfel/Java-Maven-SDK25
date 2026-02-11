002b569f09482cd959f86fb435b02f703ee2df10
Junfel <vjunfel@gmail.com>
2026-02-11 22:39:27 +0800

    Use Java I/O FileReader excercise

--- src/main/java/com/junfel/exercises/javaio/Writer.java
+++ src/main/java/com/junfel/exercises/javaio/Writer.java
@@ -7,22 +7,25 @@ public class Writer {
 
     // #Method Overloading
     // Method 1 - (filePath, TextToWrite)
-    public static void write(String filePath, String textToWrite) {
+    public static void write(String filePath, String textToWrite) throws IOException {
 
         // This format is manually close
+        FileWriter writer = null;
         try {
-            FileWriter writer = new FileWriter(filePath);
+            writer = new FileWriter(filePath);
             writer.write(textToWrite);
-            writer.close();
+
         } catch (IOException e) {
             System.out.println("Error: " + e);
+        } finally {
+            writer.close();
         }
     }
 
     // Method 2 - (filePath, TextToWrite, append)
     public static void write(String filePath, String textToWrite, boolean append) {
 
-        // This format is auto-close (Recommended)
+        // try-with-resources is auto-close (Recommended)
         try (FileWriter writer = new FileWriter(filePath, append)) {
             writer.write(textToWrite);
         } catch (IOException e) {
